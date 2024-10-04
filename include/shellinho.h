7f0fbd1260f2db3b02c5618c4cdb5c21cbee376a
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-10-04 17:13:50 +0200

    Added header for the project

--- include/shellinho.h
+++ include/shellinho.h
@@ -6,11 +6,30 @@
 /*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 18:43:40 by apetitco          #+#    #+#             */
-/*   Updated: 2024/10/03 18:44:23 by apetitco         ###   ########.fr       */
+/*   Updated: 2024/10/04 16:53:49 by apetitco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SHELLINHO_H
-#define SHELLINHO_H
+# define SHELLINHO_H
+
+# include <stdbool.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+typedef bool	t_error;
+
+# define ERROR 1
+# define NO_ERROR 0
+
+// ===== STRUCTURES =====
+typedef struct	s_shell_data
+{
+	int	last_exit_status;
+}				t_shell_data;
+
+// ===== UTILITARIES =====
+int	ft_strcmp(const char *s1, const char *s2);
 
 #endif
