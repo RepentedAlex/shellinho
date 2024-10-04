6e16d5b38e6c9e0d1bdd21e7015392828a960099
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-10-04 17:41:23 +0200

    Aligned ft_strdup and ft_strlen functions

--- src/utils1.c
+++ src/utils1.c
@@ -21,3 +21,28 @@ int		ft_strcmp(const char *s1, const char *s2)
 	}
 	return (*s1 - *s2);
 }
+
+char	*ft_strdup(const char *s)
+{
+	char	*ret;
+	int		i;
+
+	ret = (char *)malloc(sizeof(s));
+	if (!ret)
+		return (NULL);
+	i = -1;
+	while (s[++i])
+		ret[i] = s[i];
+	ret[i] = '\0';
+	return (ret);
+}
+
+size_t ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
